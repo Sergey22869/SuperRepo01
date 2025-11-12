07ab8b544e8829fc6e5d07802109c2e4a047e9e8
Sergey Ivanov <mineranem76@gmail.com>
2025-11-12 12:50:55 +0300

    v0.01

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# ----------------------------
+# Файл: calculator.py
+# ----------------------------
+
+
+def add(a: float, b: float) -> float:
+    """Возвращает сумму двух чисел"""
+    return a + b
+
+
+def subtract(a: float, b: float) -> float:
+    """Возвращает разность a - b"""
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    """Возвращает произведение"""
+    return a * b
+
+
+def divide(a: float, b: float) -> float:
+    """Возвращает результат деления a / b, если b ≠ 0"""
+    if b == 0:
+        raise ValueError("Division by zero is not allowed.")
+    return a / b
